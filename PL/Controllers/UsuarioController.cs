724627779304b1571b41165c36a37e0b405d0cb8
MauricioAlberto13 <marodriguezg01@outlook.com>
2025-10-24 17:54:49 -0600

    .

---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private readonly BL.Usuarios _usuario;
+
+        public UsuarioController(BL.Usuarios usuarios)
+        {
+        _usuario=usuarios;
+
+        }
+        public IActionResult GetAll()
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            ML.Result result = _usuario.GetAll();
+            if (result.Correct)
+            {
+                 usuario.Usuarios= result.Objects;
+            }
+            else
+            {
+                return View();
+            }
+
+            return View(usuario);
+        }
+    }
+}
